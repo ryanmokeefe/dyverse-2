6b060b0577763f872559f02fe1b5ed05c37c74e2
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-09-29 12:55:05 -0400

    backup

--- js/vid.js
+++ js/vid.js
@@ -108,3 +108,51 @@ var vids = {
 
 
 
+
+// Explosions
+
+
+
+// click event listener
+$('body').on('click', function(e) {
+    explode(e.pageX, e.pageY);
+  })
+  
+  // explosion construction
+  function explode(x, y) {
+    var particles = 55,
+
+      explosion = $('<div class="explosion"></div>');
+  
+    $('body').append(explosion);
+  
+    explosion.css('left', x - explosion.width() / 2);
+    explosion.css('top', y - explosion.height() / 2);
+  
+    for (var i = 0; i < particles; i++) {
+      
+      var x = (explosion.width() / 2) + rand(200, 250) * Math.cos(2 * Math.PI * i / rand(particles - 10, particles + 10)),
+        y = (explosion.height() / 2) + rand(200, 250) * Math.sin(2 * Math.PI * i / rand(particles - 10, particles + 10)),
+        color = rand(100, 255), 
+
+        elm = $('<div class="particle" style="' +
+          'background-color: rgb(' + color + ', ' +  color + ', ' + color + ') ;' +
+          'top: ' + y + 'px; ' +
+          'left: ' + x + 'px"></div>');
+  
+      if (i == 0) { 
+
+        elm.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+          explosion.remove(); 
+        });
+      }
+      explosion.append(elm);
+    }
+  }
+  
+  // get random number between min and max value
+  function rand(min, max) {
+    return Math.floor(Math.random() * (max + 1)) + min;
+  }
+
+
